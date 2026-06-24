1a38a1aa889c2f7aaa0f40ac5eff9b03e7109c79
steve3ussr <2816825048@qq.com>
2026-06-24 09:17:50 +0800

    release v1.0.2 with security hardening and UI refactor
    - sec(nginx): configure CF AOP to prevent direct IP scanning
    - refactor(blog-detail): migrate share-bubble to toast-msg component

--- steve_site/static/js/blog-copy-to-clp.js
+++ steve_site/static/js/blog-copy-to-clp.js
@@ -24,6 +24,8 @@ function copyCore(s) {
 }
 
 function copyLinkToClipboard(btn) {
+    if (btn.disabled) return;
+    btn.disabled = true;
     const currBlogLink = window.location.href;
     const titleElement = document.querySelector('.article-title');
     if (!titleElement) {
@@ -33,15 +35,12 @@ function copyLinkToClipboard(btn) {
     const textToCopy = `标题: [${titleElement.innerText}] 链接: ${currBlogLink}`;
 
     copyCore(textToCopy).then(() => {
-        const bubble = document.getElementById('share-bubble');
-        bubble.classList.add('show');
-        setTimeout(() => {
-            bubble.classList.remove('show');
-        }, 1000);
+        showToast('success', "复制成功", "", ()=>{btn.disabled = false;});
     }).catch((err) => {
         console.error("复制链接至剪贴板失败: ", err);
         const textarea = document.getElementById("_tmp_copy_to_clipboard_textarea");
         if (textarea) document.body.removeChild(textarea);
+        btn.disabled = false;
     });
 
 }
